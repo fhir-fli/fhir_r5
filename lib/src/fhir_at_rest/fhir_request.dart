84c6aeaa3f7d4a4b8c87c0b289f4ec1879419b9c
Dokotela <grey.faulkenberry@mayjuun.com>
2024-07-24 16:08:13 -0400

    yeah, yeah

--- lib/src/fhir_at_rest/fhir_request.dart
+++ lib/src/fhir_at_rest/fhir_request.dart
@@ -1831,22 +1831,7 @@ class FhirRequest with _$FhirRequest {
             )
           ]);
         } else if (body['resourceType'] == 'OperationOutcome') {
-          OperationOutcome operationOutcome = OperationOutcome.fromJson(
-              body['response'] as Map<String, dynamic>);
-          if (body?['status'] != null || body?['message'] != null) {
-            operationOutcome = operationOutcome.copyWith(
-              issue: <OperationOutcomeIssue>[
-                if (operationOutcome.issue.isNotEmpty)
-                  ...operationOutcome.issue,
-                OperationOutcomeIssue(
-                    severity: FhirCode('error'),
-                    code: FhirCode('unknown'),
-                    diagnostics:
-                        'Status: ${body?['status']}\nMessage: ${body?['message']}\n'),
-              ],
-            );
-          }
-          return operationOutcome;
+          return OperationOutcome.fromJson(body as Map<String, dynamic>);
         } else {
           final Resource newResource = Resource.fromJson(
               jsonDecode(result.body) as Map<String, dynamic>);
